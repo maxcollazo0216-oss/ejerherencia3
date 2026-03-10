86770e215ff0e22766ca78f292095019f7f6a2fd
maxcollazo0216@gmail.com <maxcollazo0216@gmail.com>
2026-03-10 13:41:45 -0600

    Version 2

---
+++ clases/herencia1/auto_particular.py
@@ -0,0 +1,19 @@
+from clases.herencia2.persona import Persona
+
+class AutoParticular (Persona):
+    def __init__(self, clave, nombre, edad, marca, color, placa):
+        super().__init__(clave, nombre, edad)
+        self.marca = marca
+        self.color = color
+        self.placa = placa
+    def __str__(self):
+        return super().__str__()+""+self.marca+""+self.color+""+self.placa
+    def subirseAuto(self):
+        print("subiendo al auto")
+    def arrancarAuto(self):
+        print("Encendiendo el radio")
+        print("Arrancando el auto")
+    def llegarDestino(self):
+        print("llegando al destino")
+    def bajandoAuto(self):
+        print("bajando el auto")
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from clases.herencia1.taxi import Taxi 
+from clases.herencia1.auto_particular import AutoParticular
 def main(): 
     coche = Taxi("123-GTO","Versa",1000,"123-a") 
     print(coche) 
@@ -7,5 +8,11 @@ def main():
     coche.acelerar() 
     coche.frenar() 
     coche.cobrarCuota() 
+    ap = AutoParticular("123","max",20,"volvo","plata","987-G")
+    print(ap)
+    ap.subirseAuto()
+    ap.arrancarAuto()
+    ap.llegarDestino()
+    ap.bajandoAuto()
 if __name__ == '__main__': 
     main()
